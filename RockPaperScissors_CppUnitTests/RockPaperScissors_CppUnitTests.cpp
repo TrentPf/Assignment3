41c6584837a9546f7dd203e624a8068d10873576
Trent Pfeiffer <trentpf@hotmail.com>
2024-10-30 14:24:59 -0400

    Add initial file structure for unit tests

---
+++ RockPaperScissors_CppUnitTests/RockPaperScissors_CppUnitTests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace RockPaperScissorsCppUnitTests
+{
+	TEST_CLASS(RockPaperScissorsCppUnitTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
